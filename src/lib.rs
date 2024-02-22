368631bb9478116157f149951b2f6637d2d675a2
nagi <t.29.wuq@gmail.com>
2024-02-23 03:35:18 +0900

    ADD combined test

---
+++ src/lib.rs
@@ -0,0 +1,77 @@
+#![no_std]
+#![cfg_attr(test, no_main)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+pub mod serial;
+use core::panic::PanicInfo;
+
+
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic_handler(info);
+}
+
+
+/*
+ * ==================================
+ *           custom test
+ * ==================================
+ */
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        print!("{}...\t", core::any::type_name::<T>());
+        self();
+        println!("[ok]");
+    }
+}
+
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+
+pub fn test_runner(tests: &[&dyn Testable]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic_handler(info: &PanicInfo) -> ! {
+    println!("[failed]\n");
+    println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
--- tests/boot.rs
+++ tests/boot.rs
@@ -3,36 +3,21 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(nagios::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
 use core::panic::PanicInfo;
-
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    nagios::println!("Hello World");
-
-    #[cfg(test)]
     test_main();
+
     loop {}
 }
-
-
-
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    nagios::test_panic_handler(info);
-}
-
-
-
 
 #[test_case]
 fn trivial_assertion() {
     assert_eq!(1, 1);
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    nagios::test_panic_handler(info)
 }
